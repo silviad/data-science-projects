473ff8cbbd555f388dcacb4e874f534ed688fae7
Silvia Dassie <silvia_dassie@yahoo.com>
2014-09-11 11:51:07 +0100

    add data products code

---
+++ data-science-certification/9-data-products/server.R
@@ -0,0 +1,56 @@
+library(shiny)
+library(rworldmap)
+library(RColorBrewer)
+library(UsingR)
+
+shinyServer(
+  
+  function(input, output) {
+    
+       data(wellbeing)     
+       
+       output$table = renderDataTable({
+         wellbeing
+       })    
+       
+       output$map <- renderPlot({
+           par(mfrow = c(1, 2))
+           sPDF <- joinCountryData2Map(wellbeing, joinCode = "NAME", 
+                                       nameJoinColumn = "Country")
+           colourPalette <- brewer.pal(5, 'RdPu')
+           mapParams <- mapCountryData(sPDF, nameColumnToPlot = input$Parameter, 
+                                       mapTitle = input$Parameter, 
+                                       mapRegion = "Europe",
+                                       colourPalette = colourPalette, 
+                                       addLegend = FALSE, 
+                                       missingCountryCol = "grey70",
+                                       oceanCol = "lightblue", borderCol = "black")
+           do.call(addMapLegend, c(mapParams, legendWidth = 0.5, legendMar = 2, 
+                                   legendLabels = "all"))
+           mapParams <- mapCountryData(sPDF, nameColumnToPlot = "Well.being", 
+                                       mapTitle = "Well.being", 
+                                       mapRegion = "Europe",
+                                       colourPalette = colourPalette, 
+                                       addLegend = FALSE, 
+                                       missingCountryCol = "grey70",
+                                       oceanCol = "lightblue", borderCol = "black")       
+           do.call(addMapLegend, c(mapParams, legendWidth = 0.5, legendMar = 2, 
+                                  legendLabels = "all"))
+       
+    })
+
+    output$regression = renderPlot({
+        formulaLm <- reactive({
+            paste("Well.being ~", "as.numeric(", input$Parameter, ")")
+        })    
+        with(wellbeing, {
+            fit <- lm(as.formula(formulaLm()))
+            plot(as.formula(formulaLm()), xlab = input$Parameter)
+            abline(fit, col = "purple")
+        })
+    })
+
+  }
+)
+
+
---
+++ data-science-certification/9-data-products/ui.R
@@ -0,0 +1,75 @@
+library(shiny)
+
+shinyUI(
+    navbarPage("What makes us happy?",
+        tabPanel("Documetation",
+            titlePanel("Well Being in Europe"),
+            p("The data used for this analysis is the data set wellbeing in the UsingR ",
+              "package. The data set is about what makes people happy (well being) in Europe. ",
+              "It contains 22 observations, one for each different country. ",
+              "Each observation has 12 variables described in the table below:"),
+            p("Country. Name of the country. Type: factor."),
+            p("Well.being. Well being level. Type: numeric."),
+            p("GDP. Gross domestic product per person (US$). Type: numeric."),
+            p("Equality. Equality (based on GINI index). Type: numeric. ",
+              "Value: from 0 = low equality to 100 = high equality."),
+            p("Food.consumption. Food supply (kCal per day per person). Type: numeric."),
+            p("Alcohol.consumption. Alcohol consumption (litres of pure alchohol ",
+              "per year per person). Type: numeric."),
+            p("Energy.consumption. Residential electricity use (kWh per year per person).",
+              " Type: numeric."),
+            p("Family. Fertility (children per women). Type: numeric."),
+            p("Working.hours. Average working hours per week per person. Type: numeric."),
+            p("Work.income. Hourly pay per person (US$). Type: numeric."),
+            p("Health.spending. Government health spending (% of government spend). Type: numeric."),
+            p("Military.spending. Military spending (% of GDP). Type: numeric."),
+            
+            h3("References"),
+            a("http://prcweb.co.uk/lab/what-makes-us-happy/"),
+            p(""),
+            a("http://www.nationalaccountsofwellbeing.org/")
+        ),
+        tabPanel("Data",
+            titlePanel("The dataset"),
+            p("The table below shows the wellbeing dataset."),
+            dataTableOutput('table')    
+        ),
+        tabPanel("Analysis",
+            titlePanel("The analysis"),
+            p("Exploratory analysis of the relationship between well-being and economic/social measures."),            
+            sidebarLayout(
+                sidebarPanel(     
+                    helpText("Selecting a measure on the left list below, the first map on right ",
+                             "shows the distribution of the measure selected compared to the ",
+                             "total well-being showed in the second map in the tab map. In addition the ",
+                             "tab regression shows the relationship between the measure (explanatory variable )",
+                             "and the well-being (response variable) in a linear regression model (purple line)."),
+                    radioButtons("Parameter", label ="Well-being measure",
+                         choices = c("GDP" = "GDP",
+                                     "Equality" = "Equality",
+                                     "Food Consumption" = "Food.consumption",
+                                     "Alcohol Consumption" = "Alcohol.consumption",
+                                     "Energy Consumption" = "Energy.consumption",  
+                                     "Family" = "Family",  
+                                     "Working Hours" = "Working.hours",  
+                                     "Work Income" = "Work.income",	
+                                     "Health Spending" = "Health.spending",	
+                                     "Military Spending" = "Military.spending"
+                                     )
+                        )
+                ),
+                mainPanel(
+                    tabsetPanel(type = "tabs",
+                        tabPanel("BoxPlot", plotOutput('map')),
+                        tabPanel("Regression model", plotOutput('regression'))
+                    )
+                )
+            )
+        ),
+        tabPanel("Code",
+            titlePanel("The code"), 
+            p("The source code is available in the github repository:"), 
+            a("https://github.com/silviad/data-science-projects/tree/master/data-science-certification/9-data-products")
+        )
+   )
+)

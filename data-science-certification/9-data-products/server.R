49e82682623d2176c4710f4f656125fed6707d6b
Silvia Dassie <silvia_dassie@yahoo.com>
2014-09-12 18:05:57 +0100

    version 1.1

--- data-science-certification/9-data-products/server.R
+++ data-science-certification/9-data-products/server.R
@@ -3,32 +3,34 @@ library(rworldmap)
 library(RColorBrewer)
 library(UsingR)
 
+data(wellbeing)  
+
+sPDF <- joinCountryData2Map(wellbeing, joinCode = "NAME", 
+                            nameJoinColumn = "Country")
+colourPalette <- brewer.pal(5, 'RdPu')
+
+
 shinyServer(
   
   function(input, output) {
-    
-       data(wellbeing)     
-       
+
        output$table = renderDataTable({
          wellbeing
        })    
        
        output$map <- renderPlot({
            par(mfrow = c(1, 2))
-           sPDF <- joinCountryData2Map(wellbeing, joinCode = "NAME", 
-                                       nameJoinColumn = "Country")
-           colourPalette <- brewer.pal(5, 'RdPu')
+           mapParamsWell <- mapCountryData(sPDF, nameColumnToPlot = "Well.being", 
+                                           mapTitle = "Well.being", 
+                                           mapRegion = "Europe",
+                                           colourPalette = colourPalette, 
+                                           addLegend = FALSE, 
+                                           missingCountryCol = "grey70",
+                                           oceanCol = "lightblue", borderCol = "black") 
+           do.call(addMapLegend, c(mapParamsWell, legendWidth = 0.5, legendMar = 2, 
+                                   legendLabels = "all"))
            mapParams <- mapCountryData(sPDF, nameColumnToPlot = input$Parameter, 
                                        mapTitle = input$Parameter, 
-                                       mapRegion = "Europe",
-                                       colourPalette = colourPalette, 
-                                       addLegend = FALSE, 
-                                       missingCountryCol = "grey70",
-                                       oceanCol = "lightblue", borderCol = "black")
-           do.call(addMapLegend, c(mapParams, legendWidth = 0.5, legendMar = 2, 
-                                   legendLabels = "all"))
-           mapParams <- mapCountryData(sPDF, nameColumnToPlot = "Well.being", 
-                                       mapTitle = "Well.being", 
                                        mapRegion = "Europe",
                                        colourPalette = colourPalette, 
                                        addLegend = FALSE, 
